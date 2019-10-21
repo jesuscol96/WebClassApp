5f3fdc3162e21dfd0df89866dee20f432d68b373
Colmenares <colmenares.j@pg.com>
2019-10-20 23:28:29 -0400

    Add paypal conection

--- WebClassApp/payment/urls.py
+++ WebClassApp/payment/urls.py
@@ -5,4 +5,7 @@ from . import views
 app_name = 'payment'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('process', views.payment_process, name='process'),
+    path('done', views.done, name='done'),
+    path('canceled', views.canceled, name='canceled'),
 ]
--- WebClassApp/payment/views.py
+++ WebClassApp/payment/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
-
+from paypal.standard.forms import PayPalPaymentsForm
 from django.urls import reverse
 from mainpage.models import Roles, Users_details
+from django.views.decorators.csrf import csrf_exempt
+from WebClassApp.settings import PAYPAL_RECEIVER_EMAIL
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     if request.user.is_authenticated:
@@ -39,3 +42,35 @@ def index(request):
         'is_instructor': is_instructor
     }
     return render(request,'payment/index.html',context)
+
+@csrf_exempt
+def done(request):
+    return render(request,'payment/done.html')
+
+@csrf_exempt
+def canceled(request):
+    return render(request,'payment/canceled.html')
+
+@login_required
+def payment_process(request):
+    host = request.get_host()
+
+    paypal_dict = {
+                    'business': PAYPAL_RECEIVER_EMAIL,
+                    'amount': 100,
+                    'item_name': 'course',
+                    'invoice': 1,
+                    'currency_code': 'USD',
+                    'notify_url':   'http://{}{}'.format(host,reverse('paypal-ipn')),
+                    'return_url':   'http://{}{}'.format(host,reverse('payment:done')),
+                    'cancel_return':   'http://{}{}'.format(host,reverse('payment:canceled')),
+    }
+
+    form = PayPalPaymentsForm(initial=paypal_dict)
+
+    context = { 'form': form,
+                'is_user': True,   
+
+    }
+
+    return render(request,'payment/process.html',context)
